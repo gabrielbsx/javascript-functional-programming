9bebe35cdc7e7ef0ac3780455fa08c4218dbf556
Gabriel Barbosa <gabrielturing@gmail.com>
2023-08-14 00:24:01 -0300

    feat: challange and myReduce

--- basics/reduce.js
+++ basics/reduce.js
@@ -14,17 +14,20 @@ const carrinho = [
   {
     nome: 'caneta',
     quantidade: 10,
-    preco: 7.99
+    preco: 7.99,
+    fragil: true
   },
   {
     nome: 'caderno',
     quantidade: 6,
-    preco: 21.99
+    preco: 21.99,
+    fragil: false
   },
   {
     nome: 'tesoura',
     quantidade: 3,
-    preco: 4.39
+    preco: 4.39,
+    fragil: true
   }
 ]
 
@@ -37,4 +40,42 @@ const totalGeral = carrinho
   .map(total)
   .reduce(somar, taxaInicial)
 
-console.log(totalGeral)
+// listar frageis,total do valor, media de totais
+const apenasFragil = item => item.fragil
+const valorTotal = item => item.preco * item.quantidade
+const mediaDosTotais = (acumulador, valor, indice, arr) => {
+  if (indice === arr.length - 1) {
+    return (acumulador + valor) / arr.length
+  }
+  return acumulador + valor
+}
+const mediaDoCurso = (acumulador, elemento) => {
+  const novoTotal = acumulador.total + elemento
+  const novaQuantidade = acumulador.quantidade + 1
+  return {
+    total: novoTotal,
+    quantidade: novaQuantidade,
+    media: novoTotal / novaQuantidade
+  }
+}
+const valorInicial = {
+  total: 0,
+  quantidade: 0,
+  media: 0
+}
+
+Array.prototype.myReduce = function (fn, acumulador) {
+  acumulador = acumulador ? acumulador : this[0]
+  for (let i = 0; i < this.length; i ++) {
+    acumulador = fn(acumulador, this[i], i, this)
+  }
+  return acumulador
+}
+
+const desafio = carrinho
+  .filter(apenasFragil)
+  .map(valorTotal)
+  // .reduce(mediaDosTotais)
+  .myReduce(mediaDoCurso, valorInicial)
+
+console.log(desafio)
